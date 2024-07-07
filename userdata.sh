8110f40074657b56f1df3f0d45fe95287b1d54d3
purna <purnaraomba@gmail.com>
2024-07-07 17:46:42 +0530

    Roboshop Update

--- userdata.sh
+++ userdata.sh
@@ -6,4 +6,4 @@ sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
 
 sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
 
-ansible-pull -i localhost -U https://github.com/purnavr/roboshop-ansible.git roboshop.yml -e role_name=${component} -e env=${env} >/opt/ansible.log
+ansible-pull -i localhost, -U https://github.com/purnavr/roboshop-ansible.git roboshop.yml -e role_name=${component} -e env=${env} >/opt/ansible.log
